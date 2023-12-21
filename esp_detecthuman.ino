0138becaf83211b2863433a29a6ca08356ef7074
Ngô Lê Tuyết Hoa <80042406+tuyethoa1011@users.noreply.github.com>
2023-12-21 21:41:03 +0700

    Update esp_detecthuman.ino

--- esp_detecthuman.ino
+++ esp_detecthuman.ino
@@ -1,22 +1,31 @@
+#include <ESP32Servo.h>
 
 /* Define the names for ESP32 pin for HC-SR04*/
 #define trigger_pin 5
 #define Echo_pin 18
-
+#define Servo_pin 26
 /* two variables to store duraion and distance value */
 long duration;
 int distance;
+Servo myservo;  // create servo object to control a servo
+int pos = 0;
 
 /* configure D9 and D11 as digital input and output respectively */
 void setup() {
   pinMode(trigger_pin, OUTPUT); // configure the trigger_pin(D9) as an Output
- // pinMode(LED_BUILTIN, OUTPUT); // Set the LED bulit in pin as a digital output
   pinMode(Echo_pin, INPUT); // configure the Echo_pin(D11) as an Input
   Serial.begin(115200); // Enable the serial with 9600 baud rate
+
+  // Allow allocation of all timers
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);
+  myservo.setPeriodHertz(50);    // standard 50 hz servo
+  myservo.attach(Servo_pin, 544, 2400); // attaches the servo on pin 18 to the servo object
 }
 
 void loop() {
-  
   digitalWrite(trigger_pin, LOW); //set trigger signal low for 2us
   delayMicroseconds(2);
 
@@ -29,11 +38,18 @@ void loop() {
   duration = pulseIn(Echo_pin, HIGH); // save time duration value in "duration variable
   distance= duration*0.034/2; //Convert pulse duration into distance
 
-  /* if distance greater than 10cm, turn on LED */
-  //if ( distance < 10)
-   // digitalWrite(LED_BUILTIN, HIGH);
-  //else 
-   // digitalWrite(LED_BUILTIN, LOW);
+  if(distance<50)
+  {
+    for (pos = 0; pos <= 180; pos += 3) { // goes from 0 degrees to 180 degrees
+      // in steps of 1 degree
+      myservo.write(pos);    // tell servo to go to position in variable 'pos'
+      delay(15);             // waits 15ms for the servo to reach the position
+    }
+    for (pos = 180; pos >= 0; pos -= 3) { // goes from 180 degrees to 0 degrees
+      myservo.write(pos);    // tell servo to go to position in variable 'pos'
+      delay(15);             // waits 15ms for the servo to reach the position
+    }
+  }
   // print measured distance value on Arduino serial monitor
   Serial.print("Distance: ");
   Serial.print(distance);
@@ -41,33 +57,3 @@ void loop() {
   delay(1000);
 }
 
-#include <ESP32Servo.h>
-
-Servo myservo;  // create servo object to control a servo
-int pos = 0;
-
-int servoPin = 26;
-
-void setup()
-{
-  // Allow allocation of all timers
-  ESP32PWM::allocateTimer(0);
-  ESP32PWM::allocateTimer(1);
-  ESP32PWM::allocateTimer(2);
-  ESP32PWM::allocateTimer(3);
-  myservo.setPeriodHertz(50);    // standard 50 hz servo
-  myservo.attach(servoPin, 544, 2400); // attaches the servo on pin 18 to the servo object
-}
-
-void loop() 
-{
-  for (pos = 0; pos <= 180; pos += 3) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    myservo.write(pos);    // tell servo to go to position in variable 'pos'
-    delay(15);             // waits 15ms for the servo to reach the position
-  }
-  for (pos = 180; pos >= 0; pos -= 3) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);    // tell servo to go to position in variable 'pos'
-    delay(15);             // waits 15ms for the servo to reach the position
-  }
-}
